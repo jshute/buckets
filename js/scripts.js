1597e20fc1eec5483413ca33b973d434f52b5e88
Jordan Shute <jordan.shute@icloud.com>
2014-05-16 10:57:08 -0700

    Fix that could case relationship updates to intermittently fail to update.

--- js/scripts.js
+++ js/scripts.js
@@ -6,56 +6,58 @@ jQuery(function($){
 			$("#list-table").tablesorter();
 		});
 	}
-
+	
 	$("body").on("click", "#list .entry *", function() {
 		window.location = "details?id=" + $(this).parents(".entry").attr("data-id");
 	});
-
+	
 	$("body").on("click", "#buckets .entry *", function() {
 		window.location = base + "buckets/edit?id=" + $(this).parents(".entry").attr("data-id");
 	});
-
+	
 	$("body").on("keyup", "#query-live", function() {
 		loadSearch();
 	});
-
+	
 	$("body").on("change", "#category-live", function() {
 		loadSearch();
 	});
-
+	
 	$("body").on("submit", "#search-live", function(e) {
 		e.preventDefault();
-
+		
 		loadSearch();
 	});
-
+	
 	$("body").on("keyup", "#savename", function() {
 		var text = " " + $(this).val();
-
+		
 		if ($(this).val() == "") {
 			text = "&hellip;";
 		}
-
+		
 		$(".entry-name").html(text);
 	});
-
-	$("body").on("click", "#edit .entry *", function() {
-		var $checkbox = $(this).parents(".entry").find(".row-toggle");
+	
+	$("body").on("click", "#edit .entry *", function(e) {
+		var $checkbox = $(this).find(".row-toggle");
 		
 		var checked = $checkbox.is(":checked");
-
+		
 		if (checked) {
 			$checkbox.removeProp("checked", false);
 		} else {
 			$checkbox.prop("checked", true);
 		}
-
+		
 		$.post(base + "toggle-relation", {
 			currentId: $checkbox.attr("data-current-id"),
 			relatedId: $checkbox.attr("name"),
 			value: checked
 		});
+		
+		e.stopPropagation();
 	});
-
+	
 	$(".data").tablesorter();
 });
