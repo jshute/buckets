9d0704e7b55647aabdabea5408b557c561cbaf29
Jordan Shute <jordan.shute@icloud.com>
2015-01-19 12:26:02 -0700

    Removed old inactive debug code
    Took out a line that was accidentally left in assigning a variable for testing.

--- views/login.php
+++ views/login.php
@@ -2,7 +2,6 @@
 	$this->message = 'Login not recognized. Try again or <a href="'.$base.'get-account">reset your account.</a>';
 endif ?>
 <h2>Login to Buckets</h2>
-<? $this->test = 'test' ?>
 <form method="post" action="<?=$base ?>auth/login">
 	<?=form::label('username', 'Email') ?>
 	<?=form::text('username', null, array('autofocus')) ?>
