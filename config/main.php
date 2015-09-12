21f64cdad569bfe049af8eacc08ad02cec491e54
Jordan Shute <jordys@Jordans-MacBook-Pro.local>
2015-09-11 20:42:26 -0700

    Switched buckets to use sqRoute

--- config/main.php
+++ config/main.php
@@ -3,6 +3,7 @@
 // Config settings
 return array(
 	'debug' => true,
+	
 	'sql' => array(
 		'dbname'   => 'app',
 		'username' => 'root',
@@ -12,6 +13,15 @@ return array(
 	
 	'auth' => array(
 		'login-failed-message' => 'Login not recognized. Try again or <a href="'.sq::base().'get-account">reset your account.</a>'
+	),
+	
+	'route' => array(
+		'definitions' => array(
+			'buckets/{action?}' => array('controller' => 'buckets'),
+			'auth/{action}' => array('controller' => 'auth'),
+			'{action}/{id?}' => array('controller' => 'site'),
+			'{controller?}/{action?}/{id?}'
+		)
 	)
 );
 
