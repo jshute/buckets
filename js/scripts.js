0321ddb664c96bde2085e2ff041cea7b9d920487
Jordan Shute <jordan.shute@icloud.com>
2015-12-04 16:51:41 -0700

    Updated buckets JS to work with sq updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,63 @@
 jQuery(function($){
 	function loadSearch() {
-		$search = $("#search-live");
-		$.get($search.attr("action"), $search.serialize(), function(data) {
-			$("#load-list").replaceWith(data);
-			$("#list-table").tablesorter();
+		$search = $('#search-live');
+		$.get($search.attr('action'), $search.serialize(), function(data) {
+			$('#load-list').replaceWith(data);
+			$('#list-table').tablesorter();
 		});
 	}
 	
-	$("body").on("click", "#list .entry *", function() {
-		window.location = "details?id=" + $(this).parents(".entry").attr("data-id");
+	$('body').on('click', '#list .entry *', function() {
+		window.location = sq.data.base + 'details/' + $(this).parents('.entry').attr('data-id');
 	});
 	
-	$("body").on("click", "#buckets .entry *", function() {
-		window.location = base + "buckets/edit?id=" + $(this).parents(".entry").attr("data-id");
+	$('body').on('click', '#buckets .entry *', function() {
+		window.location = sq.data.base + 'buckets/edit/' + $(this).parents('.entry').attr('data-id');
 	});
 	
-	$("body").on("keyup", "#query-live", function() {
+	$('body').on('keyup', '#query-live', function() {
 		loadSearch();
 	});
 	
-	$("body").on("change", "#category-live", function() {
+	$('body').on('change', '#category-live', function() {
 		loadSearch();
 	});
 	
-	$("body").on("submit", "#search-live", function(e) {
+	$('body').on('submit', '#search-live', function(e) {
 		e.preventDefault();
 		
 		loadSearch();
 	});
 	
-	$("body").on("keyup", "#savename", function() {
-		var text = " " + $(this).val();
+	$('body').on('keyup', '#savename', function() {
+		var text = ' ' + $(this).val();
 		
-		if ($(this).val() == "") {
-			text = "&hellip;";
+		if ($(this).val() == '') {
+			text = '&hellip;';
 		}
 		
-		$(".entry-name").html(text);
+		$('.entry-name').html(text);
 	});
 	
-	$("body").on("click", "#edit .entry *", function(e) {
-		var $checkbox = $(this).find(".row-toggle");
+	$('body').on('click', '#edit .entry *', function(e) {
+		var $checkbox = $(this).find('.row-toggle');
 		
-		var checked = $checkbox.is(":checked");
+		var checked = $checkbox.is(':checked');
 		
 		if (checked) {
-			$checkbox.removeProp("checked", false);
+			$checkbox.removeProp('checked', false);
 		} else {
-			$checkbox.prop("checked", true);
+			$checkbox.prop('checked', true);
 		}
 		
-		$.post(base + "toggle-relation", {
-			currentId: $checkbox.attr("data-current-id"),
-			relatedId: $checkbox.attr("name"),
+		$.post(base + 'toggle-relation', {
+			currentId: $checkbox.attr('data-current-id'),
+			relatedId: $checkbox.attr('name'),
 			value: checked
 		});
 		
 		e.stopPropagation();
 	});
 	
-	$(".data").tablesorter();
+	$('.data').tablesorter();
 });
